24ee017ff8876cb5e89121323c63f1b13926932f
AlexanderBenz <alexbenz7@web.de>
2024-06-26 23:08:29 +0200

    update visualization for new experiments and remove seeds from the list

--- src_shiny_app/app.py
+++ src_shiny_app/app.py
@@ -8,7 +8,7 @@ import os
 
 # available selections
 
-qs_strategies = ["random", "uncertainty_sampling", "probabilistic_active_learning"]
+qs_strategies = ["random", "UncertaintySampling", "probabilistic_active_learning"]
 models = ["logistic_regression", "parzen_window_classifier", "random_forest_tree"]
 datasets_image = ["cifar10"]
 datasets_text = ["trec6"]
@@ -27,10 +27,11 @@ selected_experiments = reactive.value([])
 
 # initial params
 
-experiment_path = "experiments/"
+experiment_path = "."
 experiments_df = reactive.value([])
+all_experiments_df = reactive.value()
 selected_df = reactive.value([])
-selected_expirements = reactive.value([])
+list_expirement_metrics = reactive.value([])
 
 
 # generate dictionaries
@@ -86,6 +87,12 @@ app_ui = ui.page_fluid(
     ui.page_fluid(
         ui.output_plot("acc_plot"),
         ui.output_plot("auroc_plot"),
+        ui.output_plot("f1_micro"),
+        ui.output_plot("f1_macro"),
+        # ui.output_plot("neg_brier_score"),
+        ui.output_plot("neg_log_loss"),
+        ui.output_plot("average_precision"),
+        ui.output_plot("balanced_accuracy"),
     )
 
     
@@ -98,13 +105,27 @@ def server(input, output, session):
     @reactive.event(input.generate_plots)
     def load_selected_df():
         expe_df = experiments_df.get().values
-        selected_dataframes_list = expe_df[list(selected_experiments.get())]
-        selected_expirements.set(selected_dataframes_list)
+        all_exp_df = all_experiments_df.get()
+        sel_dataframe = expe_df[list(selected_experiments.get())]
+        selected_dataframes_list = all_exp_df.loc[all_exp_df['dataset'].isin(sel_dataframe[:,0])]
+        selected_dataframes_list = selected_dataframes_list.loc[selected_dataframes_list['model'].isin(sel_dataframe[:,1])]
+        selected_dataframes_list = selected_dataframes_list.loc[selected_dataframes_list['qs_strategy'].isin(sel_dataframe[:,2])]
+        selected_dataframes_list = selected_dataframes_list.loc[selected_dataframes_list['batch_size'].isin(sel_dataframe[:,3])]
+        selected_dataframes_list = selected_dataframes_list.values
+        list_expirement_metrics.set(sel_dataframe)
+        
         df = []
+        old_path = ""
         for selected_dataframe in selected_dataframes_list:
-            filepath = Path(__file__).parent / experiment_path
-            filepath = filepath.__str__() + "\\" + "\\".join(selected_dataframe)
-            df.append(pd.read_csv(filepath))
+            
+            filespath = Path(__file__).parent / experiment_path
+            current_path = "\\".join(selected_dataframe[:4])
+            filespath = filespath.__str__() + "/" + "\\".join(selected_dataframe)
+            if old_path != current_path:
+                df.append([])
+            df[-1].append(pd.read_csv(filespath))
+            old_path = current_path
+            
         selected_df.set(df)
         
 
@@ -125,22 +146,20 @@ def server(input, output, session):
             df = df.loc[df['qs_strategy'].isin(selected_qs_list)]
         if len(selected_batch_sizes_list) > 0:
             df = df.loc[df['batch_size'].isin(selected_batch_sizes_list)]
-            
-        
+        all_experiments_df.set(df)
+        df = df[["dataset", "model", "qs_strategy", "batch_size"]].drop_duplicates()
 
         experiments_df.set(df)
 
     @render.text
     def value():
         # return ", ".join(input.qs_strategies()) , ", ".join(input.models())
-        return "Please load the Experiments before generating plots"
+        return "Please load the Experiments before generating plots", len(list_expirement_metrics.get())
     
     @render.data_frame
     @reactive.event(input.action_button)
     def datatable():
-        
         load_df()
-
         return render.DataTable(experiments_df.get(), selection_mode="rows") 
     
     @render.ui
@@ -150,38 +169,66 @@ def server(input, output, session):
         selected_experiments.set(rows)
         return f"Rows selected (select multiple with ctrl): {selected} "
     
+    def create_fig(metric_str, x_label="cycles", y_label="accuracy"):
+        fig, ax = plt.subplots()
+        sel_exp = list_expirement_metrics.get()
+        ax.set_title(f"{metric_str} graph")
+        ax.set_xlabel(x_label)
+        ax.set_ylabel(y_label)
+
+        for (df_list, sel) in zip(selected_df.get(),sel_exp):
+            metric = []
+            for df in df_list:
+                metric.append(df[f"{metric_str}"])
+            reshaped_result = np.array(metric).reshape((-1, len(metric[0])))
+            errorbar_mean = np.mean(reshaped_result, axis=0)
+            errorbar_std = np.std(reshaped_result, axis=0)
+            ax.errorbar(np.arange(1, len(metric[0])+1), errorbar_mean, errorbar_std, label=f"({np.mean(errorbar_mean):.4f}) {'+'.join(sel[:4])}", alpha=0.5)
+        ax.legend()
+
+        return fig
+
     @render.plot(alt="Accuracy graph")
     @reactive.event(input.generate_plots)
     def acc_plot(): 
         load_selected_df()
-        fig, ax = plt.subplots()
-        sel_exp = selected_expirements.get()
-        ax.set_title("Accuracy graph")
-        ax.set_xlabel("cycles")
-        ax.set_ylabel("accuracy")
-
-        for df in selected_df.get():
-            accuracy = df["accuracy"]
-            ax.plot(accuracy)
-        ax.legend(sel_exp[:,2])
-
-        return fig  
+        return create_fig("accuracy")
 
     @render.plot(alt="Auroc graph")
     @reactive.event(input.generate_plots)
-    def auroc_plot(): 
-        fig, ax = plt.subplots()
-        sel_exp = selected_expirements.get()
-        ax.set_title("Accuracy graph")
-        ax.set_xlabel("cycles")
-        ax.set_ylabel("auroc")
-
-        for df in selected_df.get():
-            accuracy = df["auroc"]
-            ax.plot(accuracy)
-        ax.legend(sel_exp[:,2])
-
-        return fig  
+    def auroc_plot():
+        return create_fig("auroc")  
+    
+    @render.plot(alt="f1_micro graph")
+    @reactive.event(input.generate_plots)
+    def f1_micro(): 
+        return create_fig("auroc")  
+    
+    @render.plot(alt="f1_macro graph")
+    @reactive.event(input.generate_plots)
+    def f1_macro(): 
+        return create_fig("f1_macro")  
+    
+    # @render.plot(alt="neg_brier_score graph")
+    # @reactive.event(input.generate_plots)
+    # def neg_brier_score(): 
+    #     load_selected_df()
+    #     return create_fig("neg_brier_score")  
+    
+    @render.plot(alt="neg_log_loss graph")
+    @reactive.event(input.generate_plots)
+    def neg_log_loss(): 
+        return create_fig("neg_log_loss")  
+    
+    @render.plot(alt="average_precision graph")
+    @reactive.event(input.generate_plots)
+    def average_precision(): 
+        return create_fig("average_precision")  
+    
+    @render.plot(alt="balanced_accuracy graph")
+    @reactive.event(input.generate_plots)
+    def balanced_accuracy(): 
+        return create_fig("balanced_accuracy")  
 
 
 app = App(ui=app_ui, server=server)
