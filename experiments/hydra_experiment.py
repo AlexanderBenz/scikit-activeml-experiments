1483f70aa84f0176fa96cda3322c05f24c596863
AlexanderBenz <alexbenz7@web.de>
2024-08-09 16:16:40 +0200

    update error for alce

--- experiments/hydra_experiment.py
+++ experiments/hydra_experiment.py
@@ -298,7 +298,7 @@ def my_app(cfg: DictConfig) -> None:
     else:
         clf = instantiate(cfg.model.class_definition, random_state=gen_seed(master_random_state), classes=np.arange(classes), **model_params)
     if hasattr(qs_params, "classes"):
-        qs_params["classes"]= np.arange(classes)
+        qs_params["classes"]= np.arange(classes).tolist()
     qs = SubSamplingWrapper(instantiate(cfg.query_strategy.class_definition, random_state=gen_seed(master_random_state), **qs_params), max_candidates=cfg.n_max_candidates, random_state=gen_seed(master_random_state))
     y_train = np.full(shape=y_train_true.shape, fill_value=MISSING_LABEL)
     clf.fit(X_train, y_train)
